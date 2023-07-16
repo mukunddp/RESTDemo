22d71d1c3a944668738dff13ff519a7fbba9d60d
mukunddp <mukundparve98@gmail.com>
2023-07-16 18:45:38 +0530

    Added Readme file

--- RESTDemo/restapp/urls.py
+++ RESTDemo/restapp/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('courses/', views.getcourse),
-    path('addcourse/', views.addcourse),
-    path('getonecourse/<int:pk>', views.getonecourse),
-    path('updatecourse/<int:pk>', views.updatecourse),
-    path('deletecourse/<int:pk>', views.deletecourse),
+    # CRUD Course
+    path('courses/', views.get_course),
+    path('add_course/', views.add_course),
+    path('get_one_course/<int:pk>', views.get_one_course),
+    path('update_course/<int:pk>', views.update_course),
+    path('delete_course/<int:pk>', views.delete_course),
 ]
--- RESTDemo/restapp/views.py
+++ RESTDemo/restapp/views.py
@@ -3,46 +3,48 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Course, CourseSerializer
-
-
+# CRUD
+# Read All Data
 @api_view(['GET'])
-def getcourse(request):
+def get_course(request):
     courses = Course.objects.all()
-    courseSerializer = CourseSerializer(courses, many=True)
-    return Response(courseSerializer.data)
+    course_serializer = CourseSerializer(courses, many=True)
+    print('Courses : ', course_serializer.data)
+    return Response(course_serializer.data)
 
 
 @api_view(['POST'])
-def addcourse(request):
-    courseserializer = CourseSerializer(data=request.data)
-    if courseserializer.is_valid():
-        courseserializer.save()
-        return Response(courseserializer.data)
-    return Response(courseserializer.errors)
+def add_course(request):
+    course_serializer = CourseSerializer(data=request.data)
+    if course_serializer.is_valid():
+        course_serializer.save()
+        return Response(course_serializer.data)
+    return Response(course_serializer.errors)
 
 
 @api_view(['GET'])
-def getonecourse(request, pk):
+def get_one_course(request, pk):
     try:
         course = Course.objects.get(pk=pk)
-        courseserializer = CourseSerializer(course)
-        return Response(courseserializer.data)
+        course_serializer = CourseSerializer(course)
+        return Response(course_serializer.data)
     except:
         msg = 'DATA Not Found'
         return Response(msg)
 
 
 @api_view(['PUT'])
-def updatecourse(request, pk):
+def update_course(request, pk):
     course = Course.objects.get(pk=pk)
-    courseserializer = CourseSerializer(course, data=request.data)
-    if courseserializer.is_valid():
-        courseserializer.save()
-    return Response(courseserializer.errors)
+    course_serializer = CourseSerializer(course, data=request.data)
+    if course_serializer.is_valid():
+        course_serializer.save()
+        return Response(course_serializer.data)
+    return Response(course_serializer.errors)
 
 
 @api_view(['DELETE'])
-def deletecourse(request, pk):
+def delete_course(request, pk):
     course = Course.objects.get(pk=pk)
     course.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
